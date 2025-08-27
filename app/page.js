4676942e5721933d657d6fdf9aafcb57004e5e58
SAM50x <149119430+Sam50x@users.noreply.github.com>
2025-08-27 22:43:58 +0300

    distance bar

--- app/page.js
+++ app/page.js
@@ -1,36 +1,23 @@
 'use client'
 
-import Card from "./components/card"
 import { useState } from "react"
 
 export default function Home() {
 
-  const [objectLocation, setObjectLocation] = useState('A4')
+  const [distance, setDistance] = useState(null)
 
-  const cardsTitles = [
-    'A1', 'A2', 'A3', 'A4',
-    'B1', 'B2', 'B3', 'B4',
-    'C1', 'C2', 'C3', 'C4',
-    'D1', 'D2', 'D3', 'D4',
-  ]
-
-  const handleLocationChange = (newLocation) =>{
-    setObjectLocation(newLocation)
-  }
-
-  const cardItems = cardsTitles.map((c, index) => {
-    return (
-      <div key={index} onClick={() => handleLocationChange(c)}>
-        <Card title={c} isObjectHere={objectLocation === c} />
-      </div>
-    )
-  })
+  const barWidth =
+    distance !== null
+      ? `${((50.5 - distance) / 50) * 100}%`
+      : "0%"
 
   return (
-    <div className="font-sans flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="grid grid-cols-4 grid-rows-4 gap-2 justify-center items-center bg-gray-200 p-6 rounded-2xl">
-        {cardItems}
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen w-full">
+      <div className="w-100 h-12 bg-white flex justify-start items-start">
+        <div className="h-12 bg-red-500 flex justify-start items-start" style={{ width: barWidth }}>
+        </div>
       </div>
+      <h1 className="text-white mt-6 font-bold">{distance || distance === 0 ? `Object on distance ${distance}` : 'No object detected'}</h1>
     </div>
   );
 }
